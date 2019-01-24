315712638f6d25b0a3d9be42eea485fadccfe525
iheidari <iman.heydari@gmail.com>
2019-01-23 19:32:29 -0800

    fixing failed test for App.test.js

--- src/ui/containers/app/App.test.js
+++ src/ui/containers/app/App.test.js
@@ -1,9 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Provider } from "react-redux";
+import store from "../../../configuration/store";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  const app = (
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+  ReactDOM.render(app, div);
   ReactDOM.unmountComponentAtNode(div);
 });
