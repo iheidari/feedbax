0e115270311e470c1a199f75d9e0a7743b825aa3
iheidari <iman.heydari@gmail.com>
2019-01-24 18:17:07 -0800

    Adding header and footer

--- src/ui/containers/app/App.js
+++ src/ui/containers/app/App.js
@@ -1,14 +1,22 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import Router from "./Router";
 import commonActionTypes from "../../../constants/actionTypes/common";
-import Router from './Router';
+import Header from "../../components/layout/Header";
+import Footer from "../../components/layout/Footer";
 
 class App extends Component {
   componentDidMount() {
     this.props.dispatch({ type: commonActionTypes.LOOKUPS_ASYNC });
   }
   render() {
-    return <Router />;
+    return (
+      <>
+        <Header />
+        <Router />
+        <Footer />
+      </>
+    );
   }
 }
 
