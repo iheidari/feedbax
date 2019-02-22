940de87baa3e740cd4d3e977e3bca7452070d6f4
iheidari <iman.heydari@gmail.com>
2019-02-22 07:25:36 -0800

    add simple ugly modal

--- src/stories/index.js
+++ src/stories/index.js
@@ -5,14 +5,27 @@ import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
 import { Button, Welcome } from '@storybook/react/demo';
+import Modal from '../ui/components/layout/Modal';
 
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+storiesOf('Dialog', module).add('simple Dialog', () => (
+  <Modal text='Title' onClose={action('closed')} open={true}>
+    Inside text
+  </Modal>
+));
+
+// ---demo--- need to be removed
+
+storiesOf('Welcome', module).add('to Storybook', () => (
+  <Welcome showApp={linkTo('Button')} />
+));
 
 storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
+  .add('with text', () => (
+    <Button onClick={action('clicked')}>Hello Button</Button>
+  ))
   .add('with some emoji', () => (
     <Button onClick={action('clicked')}>
-      <span role="img" aria-label="so cool">
+      <span role='img' aria-label='so cool'>
         😀 😎 👍 💯
       </span>
     </Button>
