e96751cf8ca691d18c90bf2123400d8bae8cf4b0
iheidari <iman.heydari@gmail.com>
2019-03-23 13:48:19 -0700

    travis debuging

--- src/ui/containers/app/Router.js
+++ src/ui/containers/app/Router.js
@@ -1,5 +1,4 @@
 import { Route, Switch } from 'react-router-dom';
-import Dashboard from '../Dashboard';
 import Feedback from '../Feedback';
 import Page404 from './Page404';
 
@@ -8,7 +7,6 @@ import React from 'react';
 const Router = () => {
   return (
     <Switch>
-      <Route path='/' exact component={Dashboard} />
       <Route path='/feedback' component={Feedback} />
       <Route component={Page404} />
     </Switch>
