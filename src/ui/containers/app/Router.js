ace9ec705b7def30d79e34467e1eca9a6e1680a4
iheidari <iman.heydari@gmail.com>
2019-03-29 08:49:21 -0700

    update react-script dependency

--- src/ui/containers/app/Router.js
+++ src/ui/containers/app/Router.js
@@ -1,4 +1,5 @@
 import { Route, Switch } from 'react-router-dom';
+import Dashboard from '../Dashboard';
 import Feedback from '../Feedback';
 import Page404 from './Page404';
 
@@ -7,6 +8,7 @@ import React from 'react';
 const Router = () => {
   return (
     <Switch>
+      <Route path='/' exact component={Dashboard} />
       <Route path='/feedback' component={Feedback} />
       <Route component={Page404} />
     </Switch>
