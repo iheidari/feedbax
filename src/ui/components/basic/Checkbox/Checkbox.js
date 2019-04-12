24f9db93f34e521831b8f5eb70d4d68b6ad5f011
iheidari <iman.heydari@gmail.com>
2019-04-12 09:04:33 -0700

    add label to checkbox component

--- src/ui/components/basic/Checkbox/Checkbox.js
+++ src/ui/components/basic/Checkbox/Checkbox.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import Checkbox from '@material-ui/core/Checkbox';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 
 const CustomizedCheckbox = props => {
-  const { onChange, value, ...otherProps } = props;
+  const { onChange, value, label, ...otherProps } = props;
   const booleanValue = value === undefined ? false : value;
   const onChangeWrapper = (event, checked) => {
     onChange(checked);
   };
   return (
-    <Checkbox
-      {...otherProps}
-      checked={booleanValue}
-      onChange={onChangeWrapper}
+    <FormControlLabel
+      control={
+        <Checkbox
+          {...otherProps}
+          checked={booleanValue}
+          onChange={onChangeWrapper}
+        />
+      }
+      label={label}
     />
   );
 };
