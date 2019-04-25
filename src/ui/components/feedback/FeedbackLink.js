34f221430a5131211e6ea289075a48e64b73dfa4
iheidari <iman.heydari@gmail.com>
2019-04-24 18:59:16 -0700

    commit chnage for feedback capital

--- src/ui/components/feedback/FeedbackLink.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
-const FeedbackLink = props => {
-  return <Link to={`/feedback/edit/${props._id}`}>{props.title}</Link>;
-};
-
-export default FeedbackLink;
